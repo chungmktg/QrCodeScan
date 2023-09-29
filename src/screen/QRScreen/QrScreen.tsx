bc548631802866b98e32ec4763b4ce5fe0392247
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2023-09-29 21:29:17 +0700

    Test back camera

--- src/screen/QRScreen/QrScreen.tsx
+++ src/screen/QRScreen/QrScreen.tsx
@@ -1,34 +1,47 @@
-import React, { useEffect,useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Html5QrcodeScanner, Html5QrcodeSupportedFormats } from "html5-qrcode";
-import { useNavigate } from 'react-router-dom';
-import { useDispatch } from 'react-redux'
-import { updateQrValue } from './../../redux/QrReducer'
-import './QrScreen.css';
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { updateQrValue } from "./../../redux/QrReducer";
+import "./QrScreen.css";
 
 export default function QrScreen() {
-  const navigate = useNavigate()
-  const dispatch = useDispatch()
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   useEffect(() => {
     let config = {
       fps: 5,
       qrbox: 250,
-      formatsToSupport: [ Html5QrcodeSupportedFormats.QR_CODE ]
+      formatsToSupport: [Html5QrcodeSupportedFormats.QR_CODE],
+      facingMode: "environment"
     };
-    let scanner = new Html5QrcodeScanner("reader", config, false)
-    scanner.render(onScanSuccess, onError)
+    let scanner = new Html5QrcodeScanner(
+      "reader",
+      {
+        fps: 5,
+        qrbox: 250,
+        formatsToSupport: [Html5QrcodeSupportedFormats.QR_CODE],
+      },
+      false
+    );
+    scanner.render(onScanSuccess, onError);
 
     function onScanSuccess(results: any) {
-      scanner.clear()
-      dispatch(updateQrValue(results))
-      navigate(`/phone`)
+      scanner.clear();
+      dispatch(updateQrValue(results));
+      navigate(`/phone`);
     }
 
     function onError(error: any) {
-      console.warn(`QR error = ${error}`)
+      console.warn(`QR error = ${error}`);
     }
-    
+
+    return () => {
+      scanner.clear();
+    };
   }, [navigate]);
+
 
   // Return the JSX elements to render the QR screen
   return (
@@ -38,4 +51,3 @@ export default function QrScreen() {
     </div>
   );
 }
-
